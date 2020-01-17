9196e5c1298eb95be3b611bf2baef92c999fc53b
unknown <wohe25@gmail.com>
2020-01-16 20:11:23 -0800

    created calculator and countdown java script files for lesson 2 assignments

---
+++ calculator.js
@@ -0,0 +1,9 @@
+// This is a calculator which takes four variables and perform the four basic mathematical operation
+const num1=5;
+const num2=3;
+const num3=10;
+const num4=15;
+console.log(num1+num4);
+console.log(num2*num3);
+console.log(num3/num2);
+console.log(num4-num1);
---
+++ countdown.js
@@ -0,0 +1,5 @@
+//This loop starts from number 10 and decrement by 1 until it prints 0
+for(let i=10; i>=0; i--)
+  {
+  console.log(i);
+  }
