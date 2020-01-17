8e7332add64a040121a723d8702112d02a68c03d
Jennifer Lee <lee.jennifer5221@gmail.com>
2020-01-17 00:51:01 -0800

    calculatorassignment

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let w = 0
+let x = 1
+let y = 2
+let z = 3
+
+if (12 / 4 - y === 0) {
+  console.log(w)
+} else if (w * 2 % 3 === 1) {
+  console.log(z)
+} else if (z / x === z) {
+  console.log(z)
+}
