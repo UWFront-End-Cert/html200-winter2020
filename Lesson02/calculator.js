78a1461750fbf91faa3809487464beb620dabfcf
kcbelford <kcbelford@hotmail.com>
2020-01-18 15:14:00 -0800

    console logged some math on some variables

---
+++ Lesson02/calculator.js
@@ -0,0 +1,20 @@
+let x = 13;
+let y = 25;
+let z = 10;
+let k = 19;
+console.log(x + y + z + k);
+//67
+
+let x = 13;
+let y = 25;
+let z = 10;
+let k = 19;
+console.log((y - z) / x * k);
+//21.92307692307692
+
+const x = 'I';
+const y = 'love';
+const z = 'pizza';
+const k = '!';
+console.log(x + ' ' + y + ' ' + z + k);
+//"I love pizza!"
