87f089cfe8fe5c573188d8347f6fc275b0a67de2
Jennifer Lee <lee.jennifer5221@gmail.com>
2020-01-18 07:08:36 -0800

    adding driving bonus HW

---
+++ driving.js
@@ -0,0 +1,10 @@
+let firstName = 'Jennifer ';
+let lastName = 'Lee ';
+let age = 15;
+let i = 16;
+
+if (age >= i) {
+  console.log(firstName+lastName+"can drive and has been driving for 21 years.");
+} else if (age < 16) {
+  console.log('5, '+'4, '+'3, '+'2, '+'1, ');
+}
