71e3ab0406662a1f235b7f55315ae32bbab1998d
JennH <marlowmayfield@hotmail.com>
2020-01-20 16:12:25 -0800

    added calulator.js file and made changes to it

---
+++ calculator.js
@@ -0,0 +1,15 @@
+const THEanswer = 2;
+console.log(THEanswer + 40);
+
+const couldbe = 2;
+const maybe = 4 * 10;
+console.log(couldbe + maybe);
+
+const dontknow = 7;
+const notsure = 2 + 4;
+const okayyes = notsure * 5;
+console.log(dontknow * notsure);
+
+const again = 8 * 9;
+const yup = - 30;
+console.log(again + yup);
