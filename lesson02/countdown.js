082c2a9d6a8dca2064953dd440648b20cc63f236
Oyun Batnasan <oyun.batnasan@gmail.com>
2020-01-21 22:37:01 -0800

    created code using loop to countdown

---
+++ lesson02/countdown.js
@@ -0,0 +1,19 @@
+var num = 10;
+
+for (var num; num >= 0; num--)
+
+  console.log('for (var num=10; num>=0; num--)', 'the number is', num);
+
+
+// Console
+// "for (var num=10; num>=0; num--)" "the number is" 10
+// "for (var num=10; num>=0; num--)" "the number is" 9
+// "for (var num=10; num>=0; num--)" "the number is" 8
+// "for (var num=10; num>=0; num--)" "the number is" 7
+// "for (var num=10; num>=0; num--)" "the number is" 6
+// "for (var num=10; num>=0; num--)" "the number is" 5
+// "for (var num=10; num>=0; num--)" "the number is" 4
+// "for (var num=10; num>=0; num--)" "the number is" 3
+// "for (var num=10; num>=0; num--)" "the number is" 2
+// "for (var num=10; num>=0; num--)" "the number is" 1
+// "for (var num=10; num>=0; num--)" "the number is" 0
