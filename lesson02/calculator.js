451782f586232f43f3c6059e08da02fce7635a3c
Oyun Batnasan <oyun.batnasan@gmail.com>
2020-01-21 21:24:53 -0800

    created calculator using 4 variables

---
+++ lesson02/calculator.js
@@ -0,0 +1,17 @@
+const x = 10;
+const y = 2;
+const z = 5;
+const a = 3;
+
+console.log(x + y);
+console.log(y - a);
+console.log(z * y);
+console.log(y / x);
+console.log(y % y);
+console.log(z ** a);
+console.log(a++);
+console.log(z--);
+console.log(-x);
+console.log(x + z(y - a) * a);
+console.log(2 + y * (4 - z) / y);
+console.log((y % y) + 4 - (a - y));
