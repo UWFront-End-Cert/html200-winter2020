90beb7419803f68171132957ea3fe454a2f7fda3
bdiallo10 <53807407+bdiallo10@users.noreply.github.com>
2020-01-22 21:40:17 -0800

    did the homework

---
+++ calculator.js
@@ -0,0 +1,17 @@
+///Calculator
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+
+console.log(a + b + c + d)
+//10
+console.log(a - d)
+//-3
+
+console.log(d * b)
+//8
+
+console.log ((a * c) + d)
+//7
