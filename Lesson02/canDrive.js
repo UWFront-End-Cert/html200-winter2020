cd62f9fbf20e5c646d770bc7d3811b75b503eeff
LucasCheadle <56379768+lucascheadle@users.noreply.github.com>
2020-01-22 21:43:44 -0800

    Lesson 2 assignment 4 bonus

---
+++ Lesson02/canDrive.js
@@ -0,0 +1,8 @@
+let firstName = "Bob";
+let lastName = "Rogers";
+let age = 22;
+let limit = 16;
+let x = (age - limit);
+if (age >= limit) {
+  console.log(firstName + ' ' + lastName ' you have been driving for' + x + ' years.');
+}
