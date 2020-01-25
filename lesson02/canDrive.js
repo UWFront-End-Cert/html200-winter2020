4dc0b8f889078a5e10bc8a20d9b02763818d94c5
Oyun Batnasan <oyun.batnasan@gmail.com>
2020-01-25 13:46:02 -0800

    pushing class code

---
+++ lesson02/canDrive.js
@@ -0,0 +1,25 @@
+const firstName = "Oyun";
+const lastName = "Batnasan";
+const age = 29;
+const drivingAge = 16;
+const drivingTime = age - drivingAge;
+
+
+//check to see if I'm illegal to drive
+//if I'm prin Oyun Batnasan can drive and has driven for x years
+if (age >= drivingAge) {
+  //do codes
+  console.log(firstName + ' ' + lastName + ' ' + 'can drive and has driven for ' + drivingTime + 'years')
+} else
+
+  //if I'm not legal to drive console log down from 5
+
+  console.log('5');
+console.log('4');
+console.log('3');
+console.log('2');
+console.log('1');
+
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
+}
