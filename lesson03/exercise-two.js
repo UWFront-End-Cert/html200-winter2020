b299a0285c169c0e9a3ace132e33020fd2996ccb
Oyun Batnasan <oyun.batnasan@gmail.com>
2020-01-25 12:01:31 -0800

    completed assignment3

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,7 @@
+var sentence = 'Seattle Shoreline Bellevue Lynnwood Shoreile are located in WA .'
+console.log(sentence);
+
+let arr = sentence.split(" ");
+
+console.log(arr);
+console.log(arr[2]);
