66d2fd14a6f79e6a831822e6123b2396b55d302e
adiallo792 <56573046+adiallo792@users.noreply.github.com>
2020-01-27 21:05:12 -0800

    exercise2 added

---
+++ excercise-two.js
@@ -0,0 +1,7 @@
+let sentence = "Bacon and eggs are delicious! And I loved them";
+
+let arr = sentence.split(" ");
+console.log("Let's console log all the elements first:")
+console.log(arr);
+console.log("Now Let's console log the third element:")
+console.log(arr[2]);
