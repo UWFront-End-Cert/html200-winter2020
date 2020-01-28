c17667b885036aa9ee8f4eaf42e957cdeb32945f
Presidential <presidential@Presidentials-MacBook-Pro-4.local>
2020-01-28 12:06:28 -0800

    Fix lesson

---
+++ lesson3/exercise-one.js
@@ -0,0 +1,8 @@
+let shoebrands = ['jordans', 'nike', 'adidas', 'fila', 'puma'];
+console.log(shoebrands);
+shoebrands.push('kenneth cole');
+console.log(shoebrands);
+shoebrands.splice(2,1);
+console.log(shoebrands);
+const shoebrandslist = shoebrands.join(', ');
+console.log(shoebrandslist);
