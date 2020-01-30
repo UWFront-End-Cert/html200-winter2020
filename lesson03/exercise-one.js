94954e884b70d86029d0892c011509efeb7ce326
unknown <wohe25@gmail.com>
2020-01-29 21:36:07 -0800

    submitting exercise one Lesson 03

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,11 @@
+//Creating an array with 5 string elements
+let greenVegetables=['Spinach','Kale','Collard Greens','Romaine Lettuce','Broccoli'];
+
+greenVegetables.push('Cabbage');// adding one element at the end of the array
+console.log(greenVegetables);
+
+greenVegetables.splice(2,1);// removing the third element
+console.log(greenVegetables);
+
+let greenVegetablesList=greenVegetables.join(','); //Creating comma separated string
+console.log(greenVegetablesList);
