3959e4cb5de8d4c9e3491ed71162a7095df63b03
unknown <wohe25@gmail.com>
2020-01-29 21:39:37 -0800

    added formatting

--- lesson03/exercise-one.js
+++ lesson03/exercise-one.js
@@ -1,5 +1,5 @@
 //Creating an array with 5 string elements
-let greenVegetables=['Spinach','Kale','Collard Greens','Romaine Lettuce','Broccoli'];
+let greenVegetables = ['Spinach','Kale','Collard Greens','Romaine Lettuce','Broccoli'];
 
 greenVegetables.push('Cabbage');// adding one element at the end of the array
 console.log(greenVegetables);
@@ -7,5 +7,5 @@ console.log(greenVegetables);
 greenVegetables.splice(2,1);// removing the third element
 console.log(greenVegetables);
 
-let greenVegetablesList=greenVegetables.join(','); //Creating comma separated string
+let greenVegetablesList = greenVegetables.join(','); //Creating comma separated string
 console.log(greenVegetablesList);
