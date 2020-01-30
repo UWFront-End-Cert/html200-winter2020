c12a20be2bd0de746b83952863951945f3b4eead
unknown <wohe25@gmail.com>
2020-01-29 22:02:06 -0800

    submitting lesson03 exercise-three

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,8 @@
+//Creating an array with 5 string elements
+let animals = ['cat','dog','cow','sheep','goat'];
+
+//displaying all the elements,the index and a comma
+for(let i = 0; i<animals.length; i++)
+  {
+    console.log( i + ',' + animals[i]);
+  }
