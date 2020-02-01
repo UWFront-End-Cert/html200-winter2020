1b793dd32ca1d9875b137a328133b7d98ed1fd61
Christy Lam <cyyllam@gmail.com>
2020-02-01 00:40:53 -0800

    added assignment 1 files

---
+++ Lesson04/math.js
@@ -0,0 +1,18 @@
+function squareNumber() {
+    let input = prompt('Enter a number');
+    let result = 0;
+
+    result = Number(input)*Number(input);
+
+    alert(result);
+}
+
+function multiplyNumbers() {
+    let input = prompt('Enter a number');
+    let inputTwo = prompt('Enter another number')
+    let result = 0;
+
+    result = Number(input)*Number(inputTwo);
+
+    alert(result);
+}
