20786a3c168e9f8547053587f0b15e3115ede0a2
Oyun Batnasan <oyun.batnasan@gmail.com>
2020-02-02 13:55:51 -0800

    completed regular and an extra assignment

---
+++ lesson04/extra.js
@@ -0,0 +1,11 @@
+let result;
+
+let number1 = prompt('pick your first number');
+let number2 = prompt('pick your second number');
+
+function multiplier(numberToMultiply) {
+  result = numberToMultiply;
+}
+
+multiplier(number1 * number2)
+alert(result);
---
+++ lesson04/index.js
@@ -0,0 +1,10 @@
+let result;
+
+let number = prompt('pick a number');
+
+function square(numberToSquare) {
+  result = numberToSquare * 2;
+}
+
+square(number);
+alert(result);
