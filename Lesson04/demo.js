a82f6274a8e26b91d80e4d0f62b0bd53f931bb17
LucasCheadle <56379768+lucascheadle@users.noreply.github.com>
2020-02-01 23:03:55 -0800

    Lesson04

---
+++ Lesson04/demo.js
@@ -0,0 +1,21 @@
+function numberSquarer() {
+    let input = prompt('Enter a number to be squared');
+    let numberToSqaure = 0;
+    let result = 0;
+    
+    result = Number(input) * Number(input);
+    
+    alert(result);
+}
+
+function numberMultiplier() {
+    let input1 = prompt('Enter the first number to multiply');
+    let input2 = prompt('Enter the second number to multiply');
+    let numberMultiplier = 0;
+    let answer = 0;
+
+    answer = Number(input1) * Number(input2);
+
+    alert(answer);
+}
+
