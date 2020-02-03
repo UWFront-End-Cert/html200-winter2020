0e26aa7d783ae390f5e1e3756d9b582c8344f5e2
JennH <marlowmayfield@hotmail.com>
2020-02-03 13:54:54 -0800

    modified and corrected exercise two

--- exercise-two.js
+++ exercise-two.js
@@ -1,2 +1,3 @@
-const Austin = ['it-is-a-truth', 'universally-acknowledged', 'that-a-single-man', 'in-possession-of-a-good-fortune', 'must-be-in-want-of-a-good-wife.'];
+const Austin = ['it is a truth', 'universally acknowledged', 'that a single man in possession of a good fortune', 'must be in want of a good wife.'];
+console.log(Austin);
 console.log(Austin[2]);
