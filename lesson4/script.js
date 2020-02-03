762e7b9a4b7e73cd20b3e5f6ffb2ba2c88bc9935
unknown <wohe25@gmail.com>
2020-02-02 19:19:15 -0800

    added button to html and function to the JS

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,10 +1,6 @@
-let inputNumber2 = prompt("Enter  a number");
-let inputNumber3 = prompt("Enter  another number");
-
-let product = multiplier(inputNumber2, inputNumber3);
-
-alert('The product of the two numbers is ' + product);
-
-function multiplier(num1, num2) {
-	return num1 * num2;
+function multiplier() {
+	let inputNumber1 = prompt("Enter  a number");
+	let inputNumber2 = prompt("Enter  another number");
+	let product = inputNumber1 * inputNumber2;
+	alert('The product of the two numbers is ' + product);
 }
