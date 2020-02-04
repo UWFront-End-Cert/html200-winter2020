656aa3ac622c00d80db94b7678dc975e2e2c5c07
kcbelford <kcbelford@hotmail.com>
2020-02-04 12:03:14 -0800

    added lesson 4 folders and content

---
+++ Lesson04/script.js
@@ -0,0 +1,9 @@
+function squareThisNumber() {
+  let input = prompt('Enter a number to be squared');
+  let exponent = 2;
+  let result = 0;
+
+  result = Number(input) ** exponent;
+
+  alert(result);
+}
