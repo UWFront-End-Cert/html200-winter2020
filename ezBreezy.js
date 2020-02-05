a4e8050e8381052478f302e3a74b7ea0a04539be
JennH <marlowmayfield@hotmail.com>
2020-02-04 22:51:49 -0800

    added ezBreezy.js and index.html for lesson 4

---
+++ ezBreezy.js
@@ -0,0 +1,10 @@
+//code for index - week 4 exercise
+
+function doubleLuck() {
+    let input=prompt('enter your lucky number');
+    let doubleNumber=input;
+    let result=0;
+
+    result=doubleNumber * Number(input);
+    alert(result);
+}
