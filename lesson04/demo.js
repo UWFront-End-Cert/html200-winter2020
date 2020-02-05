b58ee63cc6ab922b041d2469100b944f106ac80f
bdiallo10 <53807407+bdiallo10@users.noreply.github.com>
2020-02-04 20:56:33 -0800

    doing homework for lesson 4

---
+++ lesson04/demo.js
@@ -0,0 +1,9 @@
+function numberToSquare () {
+    let input = prompt ('Enter a number');
+    let numberToSquare = input
+    let result = 0;
+
+    result = numberToSquare * Number(input);
+
+    alert(result)
+}
