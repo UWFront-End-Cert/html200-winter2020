ee505dff1aea9a260ea69c463c55c883da463e5b
Presidential <presidential@Presidentials-MacBook-Pro-4.local>
2020-02-05 06:46:25 -0800

    Added Extra Challenge

---
+++ lesson4/js/javascriptfile.js
@@ -0,0 +1,10 @@
+let number = prompt('Enter a number');
+let multiplied = prompt('Enter a number to be multiply');
+
+function multiply(multiplyNumber) {
+  result = multiplyNumber;
+}
+
+
+multiply(number * multiplied);
+alert(result);
