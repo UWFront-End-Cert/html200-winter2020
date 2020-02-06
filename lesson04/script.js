6b690c4ab54653c8d8456acc18073ba622272e98
Taylor Sommers <taylorsommers@Taylors-MacBook-Pro.local>
2020-02-05 18:51:58 -0800

    Added Lesson04 Assignment

---
+++ lesson04/script.js
@@ -0,0 +1,21 @@
+
+function squNum() {
+    let input = prompt('Enter a number to be squared');
+    let numberToSqaure = 0;
+    let result = 0;
+
+    result = Number(input) * Number(input);
+
+    alert(result);
+}
+
+function numberMultiplier() {
+    let input1 = prompt('What is your first number?');
+    let input2 = prompt('What is your second number?');
+    let numberMultiplier = 0;
+    let answer = 0;
+
+    answer = Number(input1) * Number(input2);
+
+    alert(answer);
+}
