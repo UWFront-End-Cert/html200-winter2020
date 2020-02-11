f01992d2a4e0d13a3a9c6c90ba666033a0714d53
kcbelford <kcbelford@hotmail.com>
2020-02-10 22:31:15 -0800

    added lesson 5 files and began coding js

---
+++ Lesson05/script.js
@@ -0,0 +1,40 @@
+// prompt tells use to enter Q (quit), W (withdraw), D (deposit), B (balance), Q (quit)
+
+// if enter Q program quits
+
+// if enter W user is propted to enter an amount to withdraw - after entering amount should be
+// able to type another option
+
+// if enter D user is propted to enter an amount to deposit - after entering amount should be
+// able to type another option
+
+// if enter B user will see their balance - after viewing balance shold be able to type another option
+
+// program will loop asking for input until user enters Q to quit
+
+
+function bankApp(actions) {
+  const input = prompt(actions);
+  let balance = Number(1000);
+
+  switch (input) {
+    case 'Q':
+      alert('Thanks for banking with us!');
+      break;
+    case 'W':
+      let withdrawNum = prompt('Enter an amount to withdraw:');
+      balance -= Number(withdrawNum);
+
+      alert('Current balance: ' + '$' + balance);
+      break;
+    case 'D':
+      let depositNum = prompt('Enter an amount to deposit:');
+      balance += Number(depositNum);
+
+      alert('Current balance: ' + '$' + balance);
+      break;
+    case 'B':
+      alert('Current balance: ' + '$' + balance);
+      break;
+  }
+}
