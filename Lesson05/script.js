0ac8800e1d41c43aab20ce4f602e4cd869551e2f
Jennifer Lee <lee.jennifer5221@gmail.com>
2020-02-13 10:40:03 -0800

    updated code for bank app but still not working

--- Lesson05/script.js
+++ Lesson05/script.js
@@ -1,6 +1,4 @@
 
-let balance = 100000;
-
 //The user should see a prompt so they can type which action to perform.
 //inputOne = prompt('Welcome to Bank of Jennifer! Thank you for being one of our valued customers. What would you like to do today? Your options are:' + '\n' + 'Q(uit), W(ithdraw), D(eposit), B(alance)' + '\n' + 'Please enter one of the letters below:');
 
@@ -8,84 +6,64 @@ let balance = 100000;
 
 //The program will loop asking for input until the user enters Q.
 //Enter Q to quit (immediately quits the program).
-input = prompt('Welcome to Bank of Jennifer! Thank you for being one of our valued customers. What would you like to do today? Your options are:' + '\n' + 'Q(uit), W(ithdraw), D(eposit), B(alance)' + '\n' + 'Please enter one of the letters below:');
+let balance = 100000;
 
 //The user should see a prompt so they can type which action to perform.
-function bankApp(input) {
-  switch (result) {
-    case 'Q':
-      return 'Have a great day! Hope to see you again soon!';
-      break;
-    case 'W':
-      return 'function withdrawMoney(W)';
-      break;
-    case 'D':
-      return 'function depositMoney(D)';
-      break;
-    case 'B':
-      return 'function balance(B))';
-      break;
+function bankApp() {
+let balance = 100000;
+let choice = prompt('Welcome to Bank of Jennifer! Thank you for being one of our valued customers. What would you like to do today? Your options are:' + '\n' + 'Q(uit), W(ithdraw), D(eposit), B(alance)' + '\n' + 'Please enter one of the letters below:');
+alert(choice);
 
+  do {
+    switch (choice.toUpperCase) {
+      case 'w':
+        return 'function withdrawMoney()';
+        break;
+      case 'd':
+        return 'function depositMoney()';
+        break;
+      case 'b':
+        return 'function balance()';
+        break;
+    }
+  }
+  while (choice != 'q');
+
+
+  function withdrawMoney() {
+  let withdrawalQuery = prompt('How much would you like to withdraw?');
+  withdrawal -= balance;
+  return withdrawal;
+
+    if (withdrawal < Number(balance - 99700)) {
+      let newBalanceWithdrawal = alert('You now have ' + Number(balance - withdrawal) + 'in your account.');
+      alert(newBalanceWithdrawal);
+      alert(choice);
+  } else if (withdrawal > balance) {
+    let anotherChance = prompt('You do not have enough money. You are at risk of overdrawing and receiving an overdraft charge. Please enter another amount:');
+    alert(newBalanceWithdrawal);
+    alert(choice);
   }
 
-  //Enter W to withdraw.
-    withdrawal = 0;
+  function depositMoney() {
+    let depositQuery = prompt('How much would you like to deposit?');
+    depositMoney += balance;
+    return depositMoney;
 
-    function withdrawMoney(W) {
-
-      //The user will be prompted again to enter an amount to withdraw.
-      let withdrawal = prompt('How much would you like to withdraw?');
-      result = balance - withdrawal;
-      for (let withdrawal = 0; withdrawal < balance - 300; withdrawal++) {
-          if (withdrawal < balance - 300) {
-            alert('You now have ' + balance - 300 + 'in your account.');
-
-            //After withdrawing money, they should be able to type another option.
-            return alert(inputOne);
-            break;
-          } else if (withdrawal > balance) {
-            let secondInput = prompt('You do not have enough money. You are at risk of overdrawing and receiving an overdraft charge. Please enter another amount:');
-            result = balance - withdrawal;
-
-            //After withdrawing money, they should be able to type another option.
-          } else {
-            console.log(inputOne);
-            break;
-          }
+      if (depositMoney > 50000) {
+        let newDeposit = prompt('You have reached the deposit limit of $50,000. Please put in another amount:');
+        alert('You now have ' + Number(depositMoney) + 'in your account.');
+        alert(choice);
+      } else if (deposit <= 50000) {
+        alert('You now have ' + Number(depositMoney) + 'in your account.');
+        alert(choice);
       }
-    }
 
-     //Enter D to deposit.
-     deposit = 0;
-
-     function depositMoney(D) {
-
-       //The user will be prompted again to enter an amount to deposit.
-       let deposit = prompt('How much would you like to deposit?');
-       result = balance + deposit;
-       for (let deposit = 0; deposit <= 50000; deposit++) {
-         if (deposit <= 50000) {
-           alert('You now have ' + [balance + deposit] + 'in your account.');
-
-           //After depositing money, they should be able to type another option.
-           return alert(inputOne);
-           break;
-         if (deposit > 50000) {
-           let warning = prompt('You have reached the deposit limit of $50,000. Please put in another amount:');
-           result = balance + warning;
-
-           //After depositing money, they should be able to type another option.
-         } else {
-           console.log(inputOne);
-           break;
-         }
-         }
-       }
-     }
-     //The user will see their balance.
-     function balance(B) {
-        let currentBalance = balance - withdrawal + deposit;
-        alert(currentBalance);
-
-     }
- }
+  function balance() {
+    let newBalance = balance - Number(depositMoney) + Number(withdrawal);
+    alert('You now have ' + Number(newBalance) + 'in your account.');
+    alert(choice);
+  }
+  }
+}
+}
