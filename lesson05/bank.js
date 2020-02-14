36a5eaafdad2ad191a4c062b77a8c09742e2de30
bdiallo10 <53807407+bdiallo10@users.noreply.github.com>
2020-02-13 18:55:56 -0800

    added loop to my functions

--- lesson05/bank.js
+++ lesson05/bank.js
@@ -1,56 +1,77 @@
-// create a box that ask for customer name.
-// once they enter their name, display 1-their ballance,
 
-function YourBank() {
-  let balance = 5000
-  result = 'Your Balance is ' + "$" + balance;
-  alert(result);
+
+
+function YourBanking() {
+
+    let balance = 5000
+    let bankOpen = true;
+
+      while (bankOpen) {
+        let userInput = prompt('Enter a value: L(Logout), B(View Balance), D(Deposite), W(withdrawal)');
+
+        switch (userInput.toUpperCase()) {
+          case 'L':
+          bankOpen = false;
+            break;
+          case 'B':
+            alert('Your Balance is $' + balance);
+            break;
+          case 'D':
+            deposite()
+            break;
+          case 'W':
+            withdraw()
+            break;
+        }
+      }
+      function deposite() {
+        let depositeMessage = prompt('Enter an Amount to deposite');
+        balance = balance + Number(depositeMessage);
+        alert('Your Balance is $' + balance)
+        }
+      function withdraw() {
+        let withdrawMessage = prompt('Enter an Amount to withdraw')
+        balance = balance - Number(withdrawMessage);
+        alert('Your Balance is $' + balance)
+      }
+
 }
+  // create a box that ask for customer name.
+  // once they enter their name, display 1-their ballance,
 
-// to logout
+  function YourBank() {
+    let balance = 5000
+    result = 'Your Balance is ' + "$" + balance;
+    alert(result);
+  }
 
-function logout() {
-  result = 'Thank you for using Diallo Bank, you may now close your browser'
-  alert(result);
-}
-// withdrawal function
-function withdrawal() {
-  let balance = 5000
-  let input = prompt ('Your Balance is $5000: Enter the amount to withdraw');
-  let withdrawal = input
-  let result = 0;
+  // to logout
 
-  result =  Number(balance) - withdrawal;
+  function logout() {
+    result = 'Thank you for using Diallo Bank, you may now close your browser'
+    alert(result);
+  }
+  // withdrawal function
+  function withdrawal() {
+    let balance = 5000
+    let input = prompt ('Your Balance is $5000: Enter the amount to withdraw');
+    let withdrawal = input
+    let result = 0;
 
-  alert(result);
-}
+    result =  Number(balance) - withdrawal;
 
-
-// deposite function
-function deposite() {
-  let balance = 5000
-  let input = prompt ('Your Balance is $500. Enter the amount to deposite');
-  let deposite = input
-  let result = 0;
-
-  result = balance + Number(deposite);
-
-  alert(result);
-}
+    alert(result);
+  }
 
 
+  // deposite function
+  function deposite() {
+    let balance = 5000
+    let input = prompt ('Your Balance is $500. Enter the amount to deposite');
+    let deposite = input
+    let result = 0;
 
-// 2 promt them to enter Q to quit, W to withdraw,
-// D to deposite, B to view ballance
-//
+    result = balance + Number(deposite);
 
-// once customer enter Q (display
-// "Thank You for Choosing Bank of America")
-
-// once customer enter W--display max ballance
-// + enter amount to withdraw
-
-// once customer enter D--display deposite box
-// where user can enter a name and they can see the total
-
-// once customer enter B, display balance
+    alert(result);
+  }
