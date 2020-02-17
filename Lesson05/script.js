bd0b901dda9fd8f7a37d63daaf18dfade3d8af4a
Jennifer Lee <lee.jennifer5221@gmail.com>
2020-02-16 17:41:48 -0800

    redid JS, deposit function was working before I fixed the withdraw function, and now that withdraw function is working, deposit function is not working for some reason...

--- Lesson05/script.js
+++ Lesson05/script.js
@@ -1,73 +1,90 @@
+// Create a bank application.
+balance = 100000;
+let notQuit = true;
 
-//The user should see a prompt so they can type which action to perform.
-//inputOne = prompt('Welcome to Bank of Jennifer! Thank you for being one of our valued customers. What would you like to do today? Your options are:' + '\n' + 'Q(uit), W(ithdraw), D(eposit), B(alance)' + '\n' + 'Please enter one of the letters below:');
+do {
+  // The user should see a prompt so they can type which action to perform.
+  let choice = prompt('Welcome to Bank of Jennifer! Thank you for being one of our valued customers. What would you like to do today? Your options are:' + '\n' + 'Q(uit), W(ithdraw), D(eposit), B(alance)' + '\n' + 'Please enter one of the letters below:');
 
-//The user will have a list of 4 actions to choose from.
-
-//The program will loop asking for input until the user enters Q.
-//Enter Q to quit (immediately quits the program).
-let balance = 100000;
-
-//The user should see a prompt so they can type which action to perform.
-function bankApp() {
-let balance = 100000;
-let choice = prompt('Welcome to Bank of Jennifer! Thank you for being one of our valued customers. What would you like to do today? Your options are:' + '\n' + 'Q(uit), W(ithdraw), D(eposit), B(alance)' + '\n' + 'Please enter one of the letters below:');
-alert(choice);
-
-  do {
-    switch (choice.toUpperCase) {
+    switch (choice.toLowerCase()) {
+      // Enter Q to quit (immediately quits the program).
+      case 'q':
+        notQuit = false;
+        alert('We\'re sorry to see you go! Have a great day.');
+        break;
+      // Enter W to withdraw.
       case 'w':
-        return 'function withdrawMoney()';
+        notQuit = true;
+        withdraw();
         break;
+      // Enter D to deposit.
       case 'd':
-        return 'function depositMoney()';
+        notQuit = true;
+        deposit();
         break;
+      // Enter B to view balance.
       case 'b':
-        return 'function balance()';
+        notQuit = true;
+        alert('Your balance is ' + balance + '.');
         break;
+      default:
+        alert('Sorry, but that value is not recognized. Please try again.');
     }
+} while (notQuit);
+
+// The user will be prompted again to enter an amount to withdraw.
+function withdraw() {
+  let withdrawQuery = prompt('How much would you like to withdraw?');
+  let newBalanceWithdraw = balance - Number(withdrawQuery);
+
+    if (Number(newBalanceWithdraw) <= 0) {
+      let newWithdraw = prompt('You do not have enough money. You are at risk of overdrawing and receiving an overdraft charge. Please try again:');
+      result = balance - Number(newWithdraw);
+      alert('Your new balance is ' + '$' + Number(result) + '.');
+    } else if (Number(newBalanceWithdraw) > 300) {
+        alert('Your balance is ' + '$' + Number(newBalanceWithdraw) + '.');
+    } else if (withdrawQuery != Number(withdrawQuery)) {
+        alert('Sorry, but that value is not recognized.');
+    } else if (Number(newBalanceWithdraw) <= 300) {
+        let decision = prompt('Warning! You will have ' + '$' + Number(newBalanceWithdraw) + ' left in your account. Are you sure you want to withdraw? Write "yes" or "no".');
+          switch (decision.toLowerCase()) {
+            case 'no':
+              alert('Your new balance is ' + '$' + balance + '.');
+              break;
+            case 'yes':
+              alert('Your balance is ' + '$' + Number(newBalanceWithdraw) + '.');
+              break;
+            default:
+              alert('Sorry, but that value is not recognized.');
+          }
   }
-  while (choice != 'q');
 
+// The user will be prompted again to enter an amount to deposit.
+function deposit() {
+  let depositQuery = prompt('How much would you like to deposit?');
+  let newBalanceDeposit = balance + Number(depositQuery);
 
-  function withdrawMoney() {
-  let withdrawalQuery = prompt('How much would you like to withdraw?');
-  withdrawal -= balance;
-  return withdrawal;
-
-    if (withdrawal < Number(balance - 99700)) {
-      let newBalanceWithdrawal = alert('You now have ' + Number(balance - withdrawal) + 'in your account.');
-      alert(newBalanceWithdrawal);
-      alert(choice);
-  } else if (withdrawal > balance) {
-    let anotherChance = prompt('You do not have enough money. You are at risk of overdrawing and receiving an overdraft charge. Please enter another amount:');
-    alert(newBalanceWithdrawal);
-    alert(choice);
-  } else {
-    alert(choice);
-  }
-
-  function depositMoney() {
-    let depositQuery = prompt('How much would you like to deposit?');
-    depositMoney += balance;
-    return depositMoney;
-
-      if (depositMoney > 50000) {
-        let newDeposit = prompt('You have reached the deposit limit of $50,000. Please put in another amount:');
-        alert('You now have ' + Number(depositMoney) + 'in your account.');
-        alert(choice);
-      } else if (deposit <= 50000) {
-        alert('You now have ' + Number(depositMoney) + 'in your account.');
-        alert(choice);
-      } else {
-        alert(choice);
-      }
-
-  function balance() {
-    let newBalance = balance - Number(depositMoney) + Number(withdrawal);
-    alert('You now have ' + Number(newBalance) + 'in your account.');
-    alert(choice);
-  }
-  }
+    if (Number(depositQuery) > 50000) {
+      let newDeposit = prompt('You cannot deposit more than $50,000 in one day. Please put in another amount:');
+      result = balance + Number(newDeposit);
+      alert('Your new balance is ' + result + '.');
+    } else if (depositQuery != Number(depositQuery)) {
+      let numberDeposit = prompt('Sorry, but that is not a number. Please try again:');
+      result = balance + Number(numberDeposit);
+      alert('Your new balance is ' + result + '.');
+    } else {
+      alert('Your new balance is ' + newBalanceDeposit + '.');
+    }
 }
 }
+
+
+// The user will see their balance.
+
+// Enter W to withdraw.
+// The user will be prompted again to enter an amount to withdraw. After withdrawing money, they should be able to type another option.
+// Enter D to deposit.
+// The user will be prompted again to enter an amount to deposit. After depositing money, they should be able to type another option.
+// Enter B to view balance.
+// The user will see their balance. Afterwards, they should be able to type another option.
+// The program will loop asking for input until the user enters Q.
