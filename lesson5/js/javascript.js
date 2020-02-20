e46c221dff1ed7e0cd585df1e61f920a3875238d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-02-19 18:14:18 -0800

    dele files

--- lesson5/js/javascript.js
+++
@@ -1,8 +0,0 @@
-let results;
-let acct = prompt('Please choose from our options Q = Quit, W = Withdraw, D = Deposit, B = Balance');
-
-function doubler(numberToDouble) {
-  results = numberToDouble * 2;
-}
-doubler(acct);
-alert(results);
